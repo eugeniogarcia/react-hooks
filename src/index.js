a64da631bf4724117ab5a477ab5f0556f1eb781c
eugenio <egsmartin@gmail.com>
2022-04-21 08:13:55 +0200

    setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App.js';
 
 //Crea el elemento root en el que renderizaremos App
-const root = document.getElementById('root');
-ReactDOM
-  .unstable_createRoot(root)
-  .render(<App />);
+//const element=React.createElement('h1',{className:'greeting'},'Hello World');
+//ReactDOM.render(element,document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
